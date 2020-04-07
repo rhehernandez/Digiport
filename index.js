a6d3cd1617db60ad9ee97e926e55531f592bec33
rhehernandez <63299735+rhehernandez@users.noreply.github.com>
2020-04-07 14:38:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let name;
+let age;
+let jobRole = "student"
+
+
+
+function personalData(name, age) {
+    console.log("hello, I am " + name + " and I am " + age + "years old")
+
+}
+
+personalData("rhema", 22)
+personalData("shaz", 46)
+personalData("alex", 13)
+
+let numberOne;
+let numberTwo;
+
+function addition(numberOne, numberTwo){
+    let total = numberOne + numberTwo
+    console.log(total)
+}
+
+addition(50, 40)
+addition(30, 50)
+addition(300, 550)
+addition(330, 530)
+
